8a6a26c3e1beab6e6644e72d706401318b64e82f
Bhui <Bhui@KI840G8-2.ad.nchecr.unsw.edu.au>
2025-08-09 08:21:18 +1000

    Move negative pid to testPerson to indicate symptomatic test

--- src/sim/Runnable_ClusterModel_Viability.java
+++ src/sim/Runnable_ClusterModel_Viability.java
@@ -210,7 +210,8 @@ public class Runnable_ClusterModel_Viability extends Runnable_ClusterModel_Multi
 	}
 
 	@Override
-	protected void testPerson(int currentTime, int pid, int infIncl, int siteIncl, int[][] cumul_treatment_by_person) {
+	protected void testPerson(int currentTime, int pid_t, int infIncl, int siteIncl, int[][] cumul_treatment_by_person) {		
+		int pid = Math.abs(pid_t); // Symptomatic or one off test if PID < 0						
 		// Additional adjustment for non-viability infection
 		int[][] nv_stat = map_non_viable_inf_until.get(pid);
 		ArrayList<int[]> post_test_reset = new ArrayList<>(); // int[]{inf_id, site_id, pre_nv_stat}
